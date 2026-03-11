c4752e2e20554af3a57364e12e18c28f6f332c2d
TARUN KUMAR <tarun200804@gmail.com>
2026-03-11 09:10:45 +0530

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -1,17 +1,15 @@
 import { initializeApp } from "https://www.gstatic.com/firebasejs/10.8.0/firebase-app.js";
 import { getAuth } from "https://www.gstatic.com/firebasejs/10.8.0/firebase-auth.js";
-import { getFirestore } from "https://www.gstatic.com/firebasejs/10.8.0/firebase-firestore.js";
 
 const firebaseConfig = {
-apiKey: "YOUR_API_KEY",
-authDomain: "YOUR_PROJECT.firebaseapp.com",
-projectId: "YOUR_PROJECT_ID",
-storageBucket: "YOUR_PROJECT.appspot.com",
-messagingSenderId: "XXXX",
-appId: "XXXX"
+  apiKey: "AIzaSyBm24nwgr8xSNhXgQbW3sWJnkoH5qApd5w",
+  authDomain: "tarun-kumar-67e87.firebaseapp.com",
+  projectId: "tarun-kumar-67e87",
+  storageBucket: "tarun-kumar-67e87.appspot.com",
+  messagingSenderId: "228921642952",
+  appId: "1:228921642952:web:178f4b87cb181fd7b97afd"
 };
 
 const app = initializeApp(firebaseConfig);
 
 export const auth = getAuth(app);
-export const db = getFirestore(app);
